6ea3866c25ebdc4c8e26c8c7d876b98d272de2d5
Ram Gollapalli <nagaramaganga@gmail.com>
2026-05-28 23:52:19 +0800

    images

--- src/data/exploreAreaData.ts
+++ src/data/exploreAreaData.ts
@@ -1,18 +1,18 @@
 import { INeighbourhoodProperty } from "@/types/property-d-t";
-import exploreThumb1 from "../../public/assets/img/explore/explore-thumb-1.jpg";
-import exploreThumb2 from "../../public/assets/img/explore/explore-thumb-2.jpg";
-import exploreThumb3 from "../../public/assets/img/explore/explore-thumb-3.jpg";
-import exploreThumb4 from "../../public/assets/img/explore/explore-thumb-4.jpg";
-import exploreThumb5 from "../../public/assets/img/explore/explore-thumb-5.jpg";
-import exploreThumb6 from "../../public/assets/img/explore/explore-thumb-6.jpg";
+import exploreThumb1 from "../../public/assets/img/explore/explore-thumb-1.png";
+import exploreThumb2 from "../../public/assets/img/explore/explore-thumb-2.png";
+import exploreThumb3 from "../../public/assets/img/explore/explore-thumb-3.png";
+import exploreThumb4 from "../../public/assets/img/explore/explore-thumb-4.png";
+import exploreThumb5 from "../../public/assets/img/explore/explore-thumb-5.png";
+import exploreThumb6 from "../../public/assets/img/explore/explore-thumb-6.png";
 
 const neighbourhoodsData: INeighbourhoodProperty[] = [
   { id: 1, name: "Kuala Lumpur", image: exploreThumb1, count: 25 },
   { id: 2, name: "Penang", image: exploreThumb2, count: 25 },
-  { id: 3, name: "Johor Bahru", image: exploreThumb3, count: 25 },
-  { id: 4, name: "Malacca", image: exploreThumb4, count: 25 },
-  { id: 5, name: "Port Dickson", image: exploreThumb5, count: 25 },
-  { id: 6, name: "Terengganu", image: exploreThumb6, count: 25 },
+  { id: 3, name: "Port Dickson", image: exploreThumb3, count: 25 },
+  { id: 4, name: "Ipoh", image: exploreThumb4, count: 25 },
+  { id: 5, name: "Johor Bahru", image: exploreThumb5, count: 25 },
+  { id: 6, name: "Malacca", image: exploreThumb6, count: 25 },
 ];
 
 export default neighbourhoodsData;
